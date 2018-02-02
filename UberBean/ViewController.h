e20935aa1f8ac4a9a8fc0f077eb2a78990e20ba2
erxgithub <erxlink@gmail.com>
2018-02-02 16:50:15 -0500

    first commit

---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Eric Gregor on 2018-02-02.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UberBean/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UberBean
+//
+//  Created by Eric Gregor on 2018-02-02.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
